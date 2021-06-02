2fc14de87a963c437b26d777b165b0d741b9db58
Brumagint <brumagint@outlook.com>
2021-06-01 20:45:27 -0400

    Added Main Menu Blueprint.  Not Implemented.

--- SokpopBootleg/Intermediate/Build/Win64/SokpopBootlegEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ SokpopBootleg/Intermediate/Build/Win64/SokpopBootlegEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.28.29912
-#include "C:/Users/Brumagint/Documents/Unreal Projects/SokpopBootleg/SokpopBootleg/Intermediate/Build/Win64/SokpopBootlegEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+// Compiler: 14.28.29333
+#include "C:/Users/Bruma/Documents/Repo/SokpopBootleg/SokpopBootleg/Intermediate/Build/Win64/SokpopBootlegEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
